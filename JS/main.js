bd3442c79680bdb4ccf6f174165c60eb81e84646
Awab Abdelbagi <awaababdelbagi@gmail.com>
2025-06-19 21:22:59 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,31 @@
-@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@100;200;300;400;500;600;700&display=swap');
-* {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    font-family: "IBM Plex Sans Arabic", sans-serif;
-    user-select: none;
+// Variale
+const allCircuts = document.querySelector(".circuts");
+
+// Circuts From JSON
+let myReq = new XMLHttpRequest();
+myReq.open("GET", "../circuits.json");
+myReq.send();
+myReq.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        let jsData = JSON.parse(this.responseText);
+        jsData.forEach(e => {
+            const boxDiv = document.createElement("div");
+            boxDiv.classList.add("boxOfCircuts");
+            boxDiv.dataset.circuts = e.id;
+            boxDiv.innerHTML = `
+                <div class="before-color" style="background-color: ${e.themeColors[0]};"></div>
+                <div class="after-color" style="background-color: ${e.themeColors[1]};"></div>
+                <div class="box">
+                    <img src="${e.flag}" alt="Japan_Flag" />
+                    <h3>حلبة ${e.name}</h3>
+                    <div class="info">
+                            <p>${e.year}</p>
+                            <p>${e.length_km} km - ${e.laps} لفة</p>
+                            <p>التوقفات في السباق: ${e.estimatedStops}</p>
+                    </div>
+                </div>
+            `;
+            allCircuts.appendChild(boxDiv);
+        });
+    }
 }
